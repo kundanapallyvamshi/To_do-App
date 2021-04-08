0389ef0a5f00b093a5d7f4c5cb60875f0d632cc8
wamsi316 <wamsi316@gmail.com>
2021-04-08 12:22:35 +0530

    controller file

--- app.js
+++ app.js
@@ -62,6 +62,4 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server running on  ${PORT}`));
+app.listen(3000, console.log(`Server running on  3000`));
