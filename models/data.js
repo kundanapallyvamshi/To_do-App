1caf0f2183a15f918fbb6e99577bac20ded6ae9d
wamsi316 <wamsi316@gmail.com>
2021-04-30 22:28:39 +0530

    added isDone field

--- models/data.js
+++ models/data.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const dataSchema = new Schema({
-    text : {
-        type : String,
-        required : true
-    },
-    isDone : {
-        type : String,
-        required : true
-    },
-    username : String,
-    password : String
-},{timestamps : true});
+  text: {
+    type: String,
+    required: true
+  },
+  isDone: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  userId: {
+    type: String,
+    require: true
+  }
+}, { timestamps: true });
 
 const Data = mongoose.model('tasks', dataSchema);
 
